3b55ea9c304a8c335ada54bbca12a01e1268f13e
dingbig <dingbig@gmail.com>
2024-05-16 17:30:57 +0800

    Revert "Update ModelSelect.tsx"
    This reverts commit 9f233eba68a38c347c4e51dd581d400bc322cbca.

--- src/web/utils/ModelSelect.tsx
+++ src/web/utils/ModelSelect.tsx
@@ -1,46 +1,39 @@
-import { Button, Menu, MenuItem } from "@blueprintjs/core";
+import { Button, MenuItem } from "@blueprintjs/core";
 import { ItemPredicate, ItemRenderer, Select } from "@blueprintjs/select";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Model } from "../../electron_api";
 import { useModelList } from "./ModelList";
-import type { ItemListRendererProps } from "@blueprintjs/select";
 
+const filterModel: ItemPredicate<Model> = (query, model, _index, exactMatch) => {
+    const normalizedTitle = model.name.toLowerCase();
+    const normalizedQuery = query.toLowerCase();
 
-const getInitialContent = () => {
-    return this.state.hasInitialContent ? (
-        <MenuItem disabled={true} text={`${TOP_100_FILMS.length} items loaded.`} roleStructure="listoption" />
-    ) : undefined;
+    if (exactMatch) {
+        return normalizedTitle === normalizedQuery;
+    } else {
+        return `${model.id}. ${normalizedTitle} ${model.name}`.indexOf(normalizedQuery) >= 0;
+    }
 };
 
-const groupedItemListPredicate = (query: string, items: Model[]) => {
-    return items
-        .filter((item, index) => filterFilm(query, item, index))
-        .sort((a, b) => this.getGroup(a).localeCompare(this.getGroup(b)));
-};
-
-
-const renderGroupedItemList = (listProps: ItemListRendererProps<Model>) => {
-    const initialContent = getInitialContent();
-    const noResults = <MenuItem disabled={true} text="No results." roleStructure="listoption" />;
-
-    // omit noResults if createNewItemFromQuery and createNewItemRenderer are both supplied, and query is not empty
-    const createItemView = listProps.renderCreateItem();
-    const maybeNoResults = createItemView != null ? null : noResults;
-
-    const menuContent = renderGroupedMenuContent(listProps, maybeNoResults, initialContent);
-    if (menuContent == null && createItemView == null) {
+const renderModel: ItemRenderer<Model> = (model, { handleClick, handleFocus, modifiers, query }) => {
+    if (!modifiers.matchesPredicate) {
         return null;
     }
-    const { createFirst } = this.state;
     return (
-        <Menu role="listbox" {...listProps.menuProps} ulRef={listProps.itemsParentRef}>
-            {createFirst && createItemView}
-            {menuContent}
-            {!createFirst && createItemView}
-        </Menu>
+        <MenuItem
+            active={modifiers.active}
+            disabled={modifiers.disabled}
+            key={model.id}
+            label={model.modified}
+            onClick={handleClick}
+            onFocus={handleFocus}
+            roleStructure="listoption"
+            text={`${model.size}. ${model.name}`}
+        />
     );
 };
+
 
 export const ModelSelect: React.FC = () => {
     const modelList = useModelList();
@@ -52,12 +45,13 @@ export const ModelSelect: React.FC = () => {
         modelList.reloadModels();
     }, []);
     return (
-        <Select>
+        <Select<Model>
             items={modelList.models}
-            itemListRenderer={renderGroupedItemList}
-            itemListPredicate={groupedItemListPredicate}
+            itemPredicate={filterModel}
+            itemRenderer={renderModel}
             noResults={<MenuItem disabled={true} text="No results." roleStructure="listoption" />}
             onItemSelect={handleItemSelect}
+        >
             <Button text={selectedModel?.name} rightIcon="double-caret-vertical" />
         </Select>
     );
